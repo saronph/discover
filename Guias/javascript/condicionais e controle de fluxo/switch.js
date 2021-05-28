15b08fd06d96e53f0772ff3431a25400e8e48104
Saron Medeiros Philippi <saron.ph@gmail.com>
2021-05-27 21:20:37 -0300

    add exercicios2

---
+++ Guias/javascript/condicionais e controle de fluxo/switch.js
@@ -0,0 +1,16 @@
+let expression = 'b'
+
+switch (expression) {
+  case 'a':
+    console.log('primeira')
+     break;
+  case 'b':
+    console.log('segunda')
+     break;
+  case 'c':
+    console.log('terceira')
+     break;
+  default:
+    console.log('default')
+     break;
+}
